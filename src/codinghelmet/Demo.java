cc3151e35b5021638870795ceaeeb34030fbf8b7
bra0144 <bran0144@umn.edu>
2021-05-21 15:53:19 -0500

    making Painter optional

--- src/codinghelmet/Demo.java
+++ src/codinghelmet/Demo.java
@@ -2,14 +2,21 @@ package codinghelmet;
 
 import java.util.Comparator;
 import java.util.List;
+import java.util.Optional;
 
 public class Demo {
-    private static Painter findCheapest1(double sqMeters, List<Painter> painters) {
+    private static Optional<Painter> findCheapest1(double sqMeters, List<Painter> painters) {
         return painters.stream()
                 .filter(Painter::isAvailable)
-                .min(Comparator.comparing(painter -> painter.estimateCompensation(sqMeters)))
-                .get();
+                .min(Comparator.comparing(painter -> painter.estimateCompensation(sqMeters)));
+    }
 
+    private static Money getTotalCost(double sqMeters, List<Painter> painters) {
+        return painters.stream()
+                .filter(Painter::isAvailable)
+                .map(painter -> painter.estimateCompensation(sqMeters))
+                .reduce(Money::add)
+                .orElse(Money.ZERO);
     }
     public void run(){
 
